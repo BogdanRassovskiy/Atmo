cab79612e821e5746b7a32c44ea7848eeb31b5ab
Bogdan <131510488+BogdanRassovskiy@users.noreply.github.com>
2026-02-11 22:58:32 +0100

    123

--- bot/views.py
+++ bot/views.py
@@ -1,13 +1,19 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from asgiref.sync import async_to_sync
 from aiogram.types import Update
 from .loader import bot, dp
 import json
 
+
 @csrf_exempt
-async def telegram_webhook(request):
+def telegram_webhook(request):
     if request.method == "POST":
         data = json.loads(request.body)
         update = Update.model_validate(data)
-        await dp.feed_update(bot, update)
+
+        async_to_sync(dp.feed_update)(bot, update)
+
         return JsonResponse({"ok": True})
+
+    return JsonResponse({"error": "Only POST allowed"}, status=405)
