50f315107fe30d20e64b1cc88964532229361257
Bogdan <131510488+BogdanRassovskiy@users.noreply.github.com>
2026-02-14 22:24:45 +0100

    link

--- atmo/urls.py
+++ atmo/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from bot.views import telegram_webhook
 from bot.views import *
 
@@ -8,3 +10,9 @@ urlpatterns = [
     path("webhook/atmo/", telegram_webhook),
     path("api/weblink/", weblink),
 ]
+
+# Раздача статики в продакшене
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
